cd7b6927e7cc4e3a4eb6b44530910cffba8167d2
Alena <112772895+Alenguye582@users.noreply.github.com>
2025-01-10 20:17:08 -0800

    created algae intake (❛ ᴗ ❛) (#7)

---
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapAlgaeIntake;
+
+public class AlgaeIntake extends SubsystemBase {
+  TalonFX intakeMotor;
+
+  /** Creates a new AlgaeIntake. */
+  public AlgaeIntake() {
+    intakeMotor = new TalonFX(mapAlgaeIntake.ALGAE_MOTOR_CAN);
+  }
+
+  public void setAlgaeIntakeMotor(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
