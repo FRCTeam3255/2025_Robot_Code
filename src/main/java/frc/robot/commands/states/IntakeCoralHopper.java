7880546d6e9ef118b76a55a1403c9bc987ebf6d1
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2025-01-11 19:21:47 -0800

    Made my Autos Work (I think...)

--- src/main/java/frc/robot/commands/states/IntakeCoralHopper.java
+++ src/main/java/frc/robot/commands/states/IntakeCoralHopper.java
@@ -5,17 +5,23 @@
 package frc.robot.commands.states;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralOuttake;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class IntakeCoralHopper extends Command {
+
+  CoralOuttake subCoralOuttake;
+
   /** Creates a new IntakeCoralHopper. */
-  public IntakeCoralHopper() {
+  public IntakeCoralHopper(CoralOuttake subCoralOuttake) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.subCoralOuttake = subCoralOuttake;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
