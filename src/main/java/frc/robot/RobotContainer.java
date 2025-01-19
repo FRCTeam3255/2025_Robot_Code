ee96ab3560965ff389ed5dfd4e03b2efff1f3193
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2025-01-19 10:37:07 -0800

    Added `.asProxy()`

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -135,17 +135,17 @@ public class RobotContainer {
 
     NamedCommands.registerCommand("PlaceSequence",
         Commands.sequence(
-            TRY_SCORING_CORAL.until(() -> !hasCoralTrigger.getAsBoolean()),
+            TRY_SCORING_CORAL.asProxy().until(() -> !hasCoralTrigger.getAsBoolean()),
             Commands.waitSeconds(1.5),
-            TRY_NONE.until(() -> !hasCoralTrigger.getAsBoolean())).asProxy());
+            TRY_NONE.asProxy().until(() -> !hasCoralTrigger.getAsBoolean())).asProxy());
 
     NamedCommands.registerCommand("PrepCoralStation",
         Commands.print("Prep Coral Station"));
 
     NamedCommands.registerCommand("GetCoralStationPiece",
         Commands.sequence(
-            TRY_INTAKING_CORAL_HOPPER.until(hasCoralTrigger),
-            TRY_PREP_CORAL_L3).asProxy());
+          TRY_INTAKING_CORAL_HOPPER.asProxy().until(hasCoralTrigger),
+          TRY_PREP_CORAL_L3.asProxy()));
   }
 
   private void configureDriverBindings(SN_XboxController controller) {
