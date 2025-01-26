133ab3ad127707886a270a96c0de22e1bd70a97c
Alena <112772895+Alenguye582@users.noreply.github.com>
2025-01-25 16:08:55 -0800

    tuned constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -339,15 +339,15 @@ public final class Constants {
 
       ELEVATOR_CONFIG.Slot0.kG = 0.3; // Volts to overcome gravity
       ELEVATOR_CONFIG.Slot0.kS = 0.4; // Volts to overcome static friction
-      ELEVATOR_CONFIG.Slot0.kV = 0.0; // Volts for a velocity target of 1 rps
-      ELEVATOR_CONFIG.Slot0.kA = 0.0; // Volts for an acceleration of 1 rps/s
+      ELEVATOR_CONFIG.Slot0.kV = 0.001; // Volts for a velocity target of 1 rps
+      ELEVATOR_CONFIG.Slot0.kA = 0.001; // Volts for an acceleration of 1 rps/s
       ELEVATOR_CONFIG.Slot0.kP = 0.3;
       ELEVATOR_CONFIG.Slot0.kI = 0.0;
       ELEVATOR_CONFIG.Slot0.kD = 0.0;
       // TODO: Tune these values
-      ELEVATOR_CONFIG.MotionMagic.MotionMagicCruiseVelocity = 80;
-      ELEVATOR_CONFIG.MotionMagic.MotionMagicAcceleration = 160;
-      ELEVATOR_CONFIG.MotionMagic.MotionMagicJerk = 1600;
+      ELEVATOR_CONFIG.MotionMagic.MotionMagicCruiseVelocity = 350;
+      ELEVATOR_CONFIG.MotionMagic.MotionMagicAcceleration = 2500;
+      // ELEVATOR_CONFIG.MotionMagic.MotionMagicJerk = 1600;
     }
 
     public static final Distance CORAL_L1_HEIGHT = Units.Inches.of(9.039062);
