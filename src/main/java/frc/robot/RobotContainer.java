7ded298d474910cebac544ea564bdeb786c3d687
ACat701 <90939494+ACat701@users.noreply.github.com>
2025-01-25 23:09:43 -0800

    Create reef Override Supplier

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -136,7 +136,8 @@ public class RobotContainer {
     subDrivetrain
         .setDefaultCommand(
             new DriveManual(subDrivetrain, conDriver.axis_LeftY, conDriver.axis_LeftX, conDriver.axis_RightX,
-                conDriver.btn_LeftBumper, conDriver.btn_LeftTrigger, conDriver.btn_RightTrigger));
+                conDriver.btn_LeftBumper, conDriver.btn_LeftTrigger, conDriver.btn_RightTrigger,
+                conDriver.axis_RightY));
 
     configureDriverBindings(conDriver);
     configureOperatorBindings(conOperator);
