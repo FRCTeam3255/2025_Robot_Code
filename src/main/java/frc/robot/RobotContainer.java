0f5596a583316a774fa4e1c5c710713db381e062
Wu-Fan-529 <3681247216@qq.com>
2025-01-26 09:37:55 +0800

    Create Algae Processor Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -191,6 +191,16 @@ public class RobotContainer {
         Commands.sequence(
             TRY_INTAKING_CORAL_HOPPER.asProxy().until(hasCoralTrigger),
             TRY_PREP_CORAL_L3.asProxy()));
+
+    NamedCommands.registerCommand("CleanL3Reef", TRY_CLEANING_L3.asProxy().until(hasAlgaeTrigger));
+
+    NamedCommands.registerCommand("PrepProcessor", TRY_PREP_PROCESSOR.asProxy().until(hasAlgaeTrigger));
+
+    NamedCommands.registerCommand("ScoreAlgaeSequence",
+        Commands.sequence(
+            TRY_SCORING_ALGAE.asProxy().until(() -> !hasAlgaeTrigger.getAsBoolean()),
+            Commands.waitSeconds(1.5),
+            TRY_NONE.asProxy().until(() -> !hasAlgaeTrigger.getAsBoolean())));
   }
 
   private void configureDriverBindings(SN_XboxController controller) {
@@ -345,7 +355,7 @@ public class RobotContainer {
   public static boolean isPracticeBot() {
     return !isPracticeBot.get();
   }
-  
+
   public void updateLoggedPoses() {
     double elevatorPos, algaeAngle;
 
