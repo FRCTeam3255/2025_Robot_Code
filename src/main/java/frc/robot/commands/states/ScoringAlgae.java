daa52ba36af10c3e277c5d0a7fd6610f97cc65c0
Evan Grinnell <155865196+S0L0GUY@users.noreply.github.com>
2025-02-15 10:11:15 -0800

    Remove duplicate check for algae intake set point in ScoringAlgae command

--- src/main/java/frc/robot/commands/states/ScoringAlgae.java
+++ src/main/java/frc/robot/commands/states/ScoringAlgae.java
@@ -38,7 +38,7 @@ public class ScoringAlgae extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (globalElevator.isAtSetPoint() && subAlgaeIntake.isAtSetPoint() && subAlgaeIntake.isAtSetPoint()) {
+    if (globalElevator.isAtSetPoint() && subAlgaeIntake.isAtSetPoint()) {
       subAlgaeIntake.setAlgaeIntakeMotor(constAlgaeIntake.ALGAE_OUTTAKE_SPEED);
     }
   }
