71f99cf74907abd4f8654aa426b3de457f49d11d
Evan Grinnell <worldevan8@gmail.com>
2025-02-26 23:13:32 -0800

    Add Tickle Autos ´･ᴗ･` (#133)
    * Added Single and Double Tickle Autos
    
    * Tweaked Tickles
    
    -_-
    
    * Changed the Direction of the Tickles
    
    Now, tickle monster goes nom nom from the side.
    
    * Tuned Sideways Tickle
    
    * Added Markers
    
    * Add ForceGamePiece and PrepPlace commands to auto sequences
    
    * Add GetCoralStationPiece markers and adjust waypoint positions in path files
    
    * Refactor path data and remove unused files for path planning
    
    * Remove obsolete auto files and add new Four Piece Low + Single Tickle auto configuration
    
    * Update path coordinates and rotation values for Single Tickle configuration
    
    * Add Four Piece Low + Single Tickle auto configuration to RobotContainer
    
    * Add Four Piece High + Double Tickle auto configuration and update path data
    
    * Fix variable assignment for Four Piece High + Double Tickle and Four Piece Low + Single Tickle configurations in RobotContainer
    
    * Tickles are done >:)
    
    ---------
    
    Co-authored-by: ACat701 <90939494+ACat701@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -554,7 +554,8 @@ public class RobotContainer {
     NamedCommands.registerCommand("ForceGamePiece",
         Commands.either(
             Commands.runOnce(() -> subStateMachine.setRobotState(RobotState.HAS_CORAL))
-                .alongWith(Commands.runOnce(() -> subCoralOuttake.setHasCoral(true))),
+                .alongWith(Commands.runOnce(() -> subCoralOuttake.setHasCoral(true))
+                    .alongWith(Commands.runOnce(() -> subAlgaeIntake.setAlgaePivotAngle(constAlgaeIntake.MAX_POS)))),
             TRY_INTAKING_CORAL_HOPPER.asProxy().until(() -> subStateMachine.getRobotState() == RobotState.HAS_CORAL),
             subCoralOuttake.sensorSeesCoralSupplier()).withName("ForceGamePiece"));
 
@@ -621,7 +622,13 @@ public class RobotContainer {
         fourPieceHigh[2] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(0)); // A
         fourPieceHigh[3] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(9)); // J
         return fourPieceHigh;
-
+      case "Four_Piece_High_Double_Tickle":
+        Pair<RobotState, Pose2d>[] fourPieceHighDoubleTickle = new Pair[4];
+        fourPieceHighDoubleTickle[0] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(11)); // L
+        fourPieceHighDoubleTickle[1] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(10)); // K
+        fourPieceHighDoubleTickle[2] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(0)); // A
+        fourPieceHighDoubleTickle[3] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(9)); // J
+        return fourPieceHighDoubleTickle;
       case "Four_Piece_Low":
         Pair<RobotState, Pose2d>[] fourPieceLow = new Pair[4];
         fourPieceLow[0] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(2)); // C
@@ -629,6 +636,13 @@ public class RobotContainer {
         fourPieceLow[2] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(4)); // E
         fourPieceLow[3] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(5)); // F
         return fourPieceLow;
+      case "Four_Piece_High_Single_Tickle":
+        Pair<RobotState, Pose2d>[] fourPieceHighSingleTickle = new Pair[4];
+        fourPieceHighSingleTickle[0] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(11)); // L
+        fourPieceHighSingleTickle[1] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(10)); // K
+        fourPieceHighSingleTickle[2] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(0)); // A
+        fourPieceHighSingleTickle[3] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(9)); // J
+        return fourPieceHighSingleTickle;
       case "Algae_Net":
         Pair<RobotState, Pose2d>[] algaeNet = new Pair[1];
         algaeNet[0] = new Pair<RobotState, Pose2d>(AUTO_PREP_CORAL_4, fieldPositions.get(6)); // G
