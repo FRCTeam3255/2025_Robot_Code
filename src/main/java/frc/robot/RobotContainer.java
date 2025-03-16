e19e3d309b3c7d921eb14c556863757becb55990
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2025-03-15 21:15:53 -0700

    Wqawsedghkilrtfyujo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -279,6 +279,8 @@ public class RobotContainer {
   }
 
   private void configureDriverBindings(SN_XboxController controller) {
+    controller.btn_Back.onTrue(Commands.runOnce(() -> subDrivetrain.resetModulesToAbsolute()));
+
     controller.btn_Start
         .onTrue(TRY_CLIMBER_DEPLOYING);
 
